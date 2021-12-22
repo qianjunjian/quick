6d785f09ede2aee244b11f4f5f22b720ce2e35d4
qianjunjian <1018265043@qq.com>
2021-12-22 17:52:34 +0800

    update

--- src/components/projectLog/projectLog.vue
+++ src/components/projectLog/projectLog.vue
@@ -1,5 +1,9 @@
 <template>
     <div id="projectLog">
+        <div class="buttons">
+            <div class="btn" @click="refreshHandle"><i class="btn-icon el-icon-refresh"></i>刷新</div>
+        </div>
+        <div class="notice">温馨提示：偷懒不想做分页，暂时只显示20条最新记录！</div>
         <el-table :data="svn" style="width: 100%" max-height="500">
             <el-table-column prop="version" label="版本" width="100" />
             <el-table-column prop="user" label="用户" width="120" />
@@ -10,7 +14,7 @@
 </template>
 
 <script>
-import { watchEffect, reactive, nextTick } from 'vue';
+import { watchEffect, reactive } from 'vue';
 import { getSvnLog } from '../../node/svn';
 
 export default {
@@ -20,17 +24,30 @@ export default {
     },
     setup(props) {
         const svn = reactive([]);
+        let timeout = null;
         watchEffect(() => {
             if (props.project && props.project.url) {
-                nextTick(() => {
+                timeout && clearTimeout(timeout);
+                timeout = setTimeout(() => {
                     svn.splice(0, svn.length);
                     svn.push(...(getSvnLog(props.project.url) || []));
-                });
+                    timeout && clearTimeout(timeout);
+                }, 100);
             }
         });
 
+        const refreshHandle = () => {
+            svn.splice(0, svn.length);
+            timeout && clearTimeout(timeout);
+            timeout = setTimeout(() => {
+                svn.push(...(getSvnLog(props.project.url) || []));
+                timeout && clearTimeout(timeout);
+            }, 100);
+        };
+
         return {
-            svn
+            svn,
+            refreshHandle
         };
     }
 };
@@ -40,5 +57,36 @@ export default {
 #projectLog {
     width: 100%;
     color: #CCCCCC;
+
+    .notice {
+        text-align: left;
+        font-size: 13px;
+        margin-top: 10px;
+    }
+
+    .buttons {
+        display: flex;
+        margin-top: 10px;
+        cursor: pointer;
+
+        .btn {
+            font-size: 12px;
+            color: #fff5e5;
+            background-color: #0e639c;
+            padding: 4px 10px;
+
+            &:not(:last-child) {
+                margin-right: 10px;
+            }
+
+            &:hover {
+                background-color: #1177bb !important;
+            }
+
+            &:active {
+                background-color: #1177bb !important;
+            }
+        }
+    }
 }
 </style>
--- src/node/svn.js
+++ src/node/svn.js
@@ -34,3 +34,32 @@ export function getSvnLog(url) {
     }
     return out;
 }
+
+export function getCurrentVer(url) {
+    const out = [];
+    const path = getSrcPath(url);
+    const logs = execSync(`cd /D ${path} && svn log -l 1`, { encoding: binaryEncoding});
+    const log = iconv.decode(new Buffer(logs, binaryEncoding), encoding);
+    if (log) {
+        let lst = log.split('------------------------------------------------------------------------');
+        if (lst) {
+            for (let i = 0; i < lst.length; i ++) {
+                let item = lst[i];
+                if (item) {
+                    item = item.replace(/\r\n/g, '');
+                    item = item.replace(/1 line/g, '');
+                    item = item.replace(/2 line/g, '');
+                    item = item.split('|');
+                    let data = {
+                        version: item[0],
+                        user: item[1],
+                        time: item[2],
+                        remark: item[3]
+                    };
+                    out.push(data);
+                }
+            }
+        }
+    }
+    return out;
+}
